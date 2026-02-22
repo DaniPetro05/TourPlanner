2a83fa33ea2d6624d87fece158be4db363a30512
Daniel Petrovic <muumipoika05@gmail.com>
2026-02-22 22:58:44 +0100

    Core logic of TourPlanner

---
+++ AngularTourPlanner/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiURL: 'https://http://localhost:5270'
+};
---
+++ TourPlanner/Program.cs
@@ -0,0 +1,60 @@
+namespace TourPlanner;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        
+        var allowedOrigins = builder.Configuration.GetValue<string>("allowedOrigins")!.Split(",");
+        
+        builder.Services.AddCors(options => 
+            {
+                options.AddDefaultPolicy(policy => 
+                {
+                    policy.WithOrigins(allowedOrigins).AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+        
+        app.UseCors();
+
+        app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        {
+                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            TemperatureC = Random.Shared.Next(-20, 55),
+                            Summary = summaries[Random.Shared.Next(summaries.Length)]
+                        })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+
+        app.Run();
+    }
+}
